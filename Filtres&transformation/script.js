24578a7b0184913b868eb9731e6db64ed27c5a7d
solenn.B <solenn.baer@gmail.com>
2016-09-20 15:19:45 +0200

    modifications

--- Filtres&transformation/script.js
+++ Filtres&transformation/script.js
@@ -1,11 +1,10 @@
+var selector = document.getElementById("scale");
 
-
-function animatecur(i) {
-  document.getElementsByClassName("listcontent")[i].style.height = "200px" ;
-  console.log("something");
-}
-function animateremove(){
-  document.getElementsByClassName("listcontent")[0].style.height = "0px" ;
-  document.getElementsByClassName("listcontent")[1].style.height = "0px" ;
-
+function hidecontent() {
+  if selector.style.height == "50px" {
+    selector.style.height = "100%" ;
+  }
+  else {
+    selector.style.height = "50px";
+  }
 }
