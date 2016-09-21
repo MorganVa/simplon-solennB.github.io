9a604d80d85784b99727859f2e13325dc2b882a9
solenn.B <solenn.baer@gmail.com>
2016-09-21 16:20:13 +0200

    modifications

--- Filtres&transformation/script.js
+++ Filtres&transformation/script.js
@@ -1,10 +1,12 @@
-var selector = document.getElementById("scale");
+var selection ;
 
-function hidecontent() {
-  if selector.style.height == "50px" {
-    selector.style.height = "100%" ;
-  }
-  else {
-    selector.style.height = "50px";
-  }
+function hideshow(sel) {
+  selection = document.getElementsByClassName("hiddencontent")[sel] ;
+  console.log(selection.style.height);
+  if (selection.style.height == "0px" || selection.style.height == "") {
+  selection.style.height = "100%" ;
+}
+else {
+  selection.style.height = "0" ;
+}
 }
