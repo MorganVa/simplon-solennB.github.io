e0552261c2ced374600672072672656c41cd3d1d
solenn.B <solenn.baer@gmail.com>
2016-09-22 09:44:56 +0200

    correction

--- Filtres&transformation/script.js
+++ Filtres&transformation/script.js
@@ -5,8 +5,10 @@ function hideshow(sel) {
   console.log(selection.style.height);
   if (selection.style.height == "0px" || selection.style.height == "") {
   selection.style.height = "100%" ;
+  selection.style.overflow = "visible" ;
 }
 else {
   selection.style.height = "0" ;
+  selection.style.overflow = "hidden" ;
 }
 }
